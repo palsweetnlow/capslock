fe1a299ca1d093d698ed31b3aaad3a2bbcebe829
Yoshihiro Satoh <64654548+palsweetnlow@users.noreply.github.com>
2026-07-15 23:46:40 +0900

    Add files via upload

---
+++ CapsLock-OFF.bat
@@ -0,0 +1,32 @@
+echo off
+
+set "needPriv=No"
+if %needPriv%==Yes (
+ :: 管理者権限の確認と昇格処理
+ openfiles >nul 2>&1
+ if errorlevel 1 (
+     echo 権限を昇格して再起動します...
+     :: PowerShellを使用して自身を管理者権限で起動
+     powershell -Command "Start-Process '%~f0' -Verb RunAs"
+     exit /b
+ )
+)
+
+powershell -Command "(New-Object -ComObject SAPI.SpVoice).Speak(\"キャップスキーがオフになりました\")"
+
+REM ここに実行するコマンドを書く。
+REM 以下の例は、パソコンのカバーを閉じたときの動作をスリープにする設定です
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION 1
+powercfg /setacvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION 1
+powercfg /setactive SCHEME_CURRENT
+
+REM このファイルと同じファイル名の Powershellスクリプト(.ps1) を起動できる
+REM runPowershell が No なら起動しない
+REM runPowershell が Yes なら普通に起動する
+REM runPowershell が Priv なら管理者で起動する
+set "runPowershell=No"
+if %runPowershell%==Yes (
+  powershell -NoProfile -ExecutionPolicy Unrestricted .\%~n0.ps1
+) else if %runPowershell%==Priv (
+  powershell -Command "Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0%~n0.ps1\"' -Verb RunAs"
+)
